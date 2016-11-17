84e5f9e0fbe482a6a20f1dc86da887e0dd7de426
tomonobu <bokotomo@me.com>
2016-11-18 03:32:16 +0900

    edit index.php

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * [Naming conventions]
+ * function : camelCase  (Make the beginning of a word a verb / 単語の先頭を動詞にする)
+ * method   : camelCase  (Make the beginning of a word a verb / 単語の先頭を動詞にする)
+ * Class    : PascalCase (Make the beginning of a word a noun / 単語の先頭を名詞にする)
+ * Variable : PascalCase (Make the beginning of a word a noun / 単語の先頭を名詞にする)
+ * DEFINE   : UPPERCASE_SNAKECASE
+ * 
+ * 2016 Fukumoto
+ * E-mail : bokotomo@me.com
+ */
 
 require_once(__DIR__."/config.php");
-require_once(__DIR__."/line-bot-sdk-php/vendor/autoload.php");
+require_once(__DIR__."/vendor/autoload.php");
 require_once(__DIR__."/sayalib/autoload.php");
 
 $InputData = file_get_contents("php://input");
@@ -10,25 +21,23 @@ $ReplyToken = $ReceiveData->events[0]->replyToken;
 $MessageType = $ReceiveData->events[0]->message->type;
 
 if($MessageType == "text"){
-  $TextMessageControllor = new TextMessageControllor($ReplyToken,$ReceiveData);
+  $TextMessageControllor = new TextMessageControllor($ReplyToken, $ReceiveData);
   $TextMessageControllor->responseMessage();
 }else if($MessageType == "sticker"){
-  $StickerMessageControllor = new StickerMessageControllor($ReplyToken,$ReceiveData);
+  $StickerMessageControllor = new StickerMessageControllor($ReplyToken, $ReceiveData);
   $StickerMessageControllor->responseMessage();
 }else if($MessageType == "image"){
-  $ImageMessageControllor = new ImageMessageControllor($ReplyToken,$ReceiveData);
+  $ImageMessageControllor = new ImageMessageControllor($ReplyToken, $ReceiveData);
   $ImageMessageControllor->responseMessage();
 }else if($MessageType == "location"){
-  $LocationMessageControllor = new LocationMessageControllor($ReplyToken,$ReceiveData);
+  $LocationMessageControllor = new LocationMessageControllor($ReplyToken, $ReceiveData);
   $LocationMessageControllor->responseMessage();
 }
 
-$file = __DIR__.'/line.html';
-$current = file_get_contents($file);
-$current .= "<div style='font-size:14px;background:#f2f2f2;margin-bottom:10px;padding:10px;'>";
-$current .= "<p>".date("Y-m-d H:i:s")."</p>";
-$current .= json_encode($ReceiveData);
-$current .= "</div>";
+$ReceiveDataLogger = new ReceiveDataLogger();
+$ReceiveDataLogger->setFilePath(__DIR__."/line.html");
+$ReceiveDataLogger->setLogType("html");
+$ReceiveDataLogger->setLogData($InputData);
+$ReceiveDataLogger->outputLog();
 
-file_put_contents($file, $current);
 ?>
