7dba621beff5994f51239de48f05a05361a28e45
tomonobu <bokotomo@me.com>
2016-11-19 17:21:13 +0900

    edit imagebuilder

--- sayalib/controller/response_message/response_image_message.php
+++ sayalib/controller/response_message/response_image_message.php
@@ -5,6 +5,7 @@ use Saya\MessageControllor;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 
 class ImageMessageControllor
@@ -23,12 +24,17 @@ class ImageMessageControllor
 //  $LineMessage->uploadImage($MessageId);
 //  $LineMessage->ImageVoteConfirmMessage();
 
+    $OriginalContentSSLUrl = "https://tomo.syo.tokyo/openimg/shibuya.jpg";
+    $PreviewImageSSLUrl = "https://tomo.syo.tokyo/openimg/shibuya.jpg";
+    $ImageMessage = new ImageMessageBuilder($OriginalContentSSLUrl, $PreviewImageSSLUrl);
+
     $TextMessageBuilder = new TextMessageBuilder("ありがと！☆^^ なかなかいいね！参考になる！");
+
     $message = new MultiMessageBuilder();
+    $message->add($ImageMessage);
     $message->add($TextMessageBuilder);
     $response = $this->Bot->replyMessage($this->ReplyToken, $message);
-
-  }  
+  } 
  
-} 
+}
 ?>
