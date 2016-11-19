3247be126f412854ccb0b6363811d5777af8ba3b
tomonobu <bokotomo@me.com>
2016-11-19 23:31:27 +0900

    edit fileupload class

--- tomolib/autoload.php
+++ tomolib/autoload.php
@@ -1,4 +1,5 @@
 <?php
   require_once(__DIR__."/logger/data_logger.php");
   require_once(__DIR__."/database/database_provider.php");
+  require_once(__DIR__."/fileupload/upload_provider.php");
 ?>
---
+++ tomolib/fileupload/upload_provider.php
@@ -0,0 +1,19 @@
+<?php
+namespace TomoLib;
+use TomoLib;
+
+class UploadProvider
+{
+  private $SqlType;
+  private $SqlPath;
+  private $PDO;
+
+  public function __construct($SqlType, $SqlPath){
+  }
+
+  public function uploadfile($SqlStr){
+
+  }
+
+} 
+?>
