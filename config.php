980bf349b0c91144a05daf5845ccb5252420e0a4
tomonobu <bokotomo@me.com>
2016-11-23 00:01:57 +0900

    edit readme

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define("SECRET_TOKEN", '');
 define("ACCESS_TOKEN", '');
-define("URL_ROOT_PATH", "https://tomo.syo.tokyo");
+define("URL_ROOT_PATH", "https://domain.com");
 define("ROOT_DIR_PATH", __DIR__);
